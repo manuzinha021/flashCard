e587ca3f91f21367d7bb65b5e9d5961a9807e1bc
manuzinha021 <146237656+manuzinha021@users.noreply.github.com>
2024-11-25 11:44:46 -0300

    .

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function criaCartao(categoria, pergunta, resposta) {
         `
         let respostaEstaVisivel = false
         function viraCartao() {
-        respostaEstaVisivel = !respostaEstaVisivel
+        respostaestaVisivel = !respostaEstaVisivel
         cartao.classList.toggle('active', respostaEstaVisivel)
         }
         cartao.addEventListener('click', viraCartao)
