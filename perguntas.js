314028db884cececbec643ce789520c8d0648fbc
manuzinha021 <146237656+manuzinha021@users.noreply.github.com>
2024-11-25 10:00:48 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -2,16 +2,33 @@ criaCartao(
     'Programação',
     'O que é Python?',
     'O Python é uma linguagem de programação'
-)
-
-criaCartao(
+    )
+    
+    criaCartao(
     'Geografia',
     'Qual a capital da França?',
     'A capital da França é Paris'
-)
-
-criaCartao(
+    )
+    
+    criaCartao(
     'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
-)
+    )
+    criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+    )
+    
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+    <p>${pergunta}</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+    <p>${resposta}</p>
+    </div>
+    </div>
+    `
